c00714168e3b473be99bbcaa6829798429c2b1e8
Susanmwende <mwendezmasaku@gmail.com>
2024-06-18 13:19:52 +0300

    About Pull dev code

--- loop-and-wear/src/App.js
+++ loop-and-wear/src/App.js
@@ -1,11 +1,14 @@
+
 import Products from './Products';
 import Homepage from "./Homepage";
-import OurTeam from "./OurTeam"
+import OurTeam from "./OurTeam";
+import About from "./About";
 
 const App = () => {
 return(
   <div>
      <Homepage/>
+     <About/>
     <Products/>
     <OurTeam/>
   </div>
