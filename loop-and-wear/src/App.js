c7159a4439dd5f1c7f264edf47675f55799ff726
Susanmwende <mwendezmasaku@gmail.com>
2024-07-26 17:17:01 +0300

    changes

--- loop-and-wear/src/App.js
+++ loop-and-wear/src/App.js
@@ -1,9 +1,17 @@
 import About from "./About"
+import Homepage from "./Homepage";
+import Products from "./Products"
+import OurTeam from "./OurTeam"
 
 function App() {
   return (
     <div className="App">
+      
+      <Homepage/>
       <About/>
+      <Products/>
+      <OurTeam/>
     </div>
   );
 }
+export default App
